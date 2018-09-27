dae7b0f1dcbb6c1dbf3535569c31286ecfe94be4
viorelcojocaru <viorelcojocaru2008@gmail.com>
2018-09-27 13:13:32 +0300

    first

--- src/main/java/com/leroiv/familyTree/domain/Relation.java
+++ src/main/java/com/leroiv/familyTree/domain/Relation.java
@@ -1,21 +1,29 @@
 package com.leroiv.familyTree.domain;
 
-/*@Data
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 //@AllArgsConstructor("a")
 @Entity(name = "familyTree")
-@Table(name = "relation")*/
-public class Relation {//extends Base
-   /* @OneToMany(targetEntity = AppAccount.class)
-    @JoinColumn(name = "from_AppAccount_Id", referencedColumnName = "id")
+@Table(name = "relation")
+public class Relation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id",unique = true )
+    private long id;
+    @ManyToOne(targetEntity = AppAccount.class)
+    @JoinColumn(name = "from_app_account_id", referencedColumnName = "id")
     private AppAccount fromAppAccountId;
-    @OneToMany(targetEntity = AppAccount.class)
-    @JoinColumn(name = "to_AppAccount_Id", referencedColumnName = "id")
+    @ManyToOne(targetEntity = AppAccount.class)
+    @JoinColumn(name = "to_app_account_id", referencedColumnName = "id")
     private AppAccount toAppAccountId;
-    @Column(name = "START_DATE")
+    @Column(name = "start_date")
     private Date startDate;
-    @Column(name = "END_DATE")
+    @Column(name = "end_date")
     private Date endDate;
-    private String location;*/
-
+    private String location;
 }
